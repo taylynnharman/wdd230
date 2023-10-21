be27616d072bb9f2957b0b4481cded267bfd8ef5
taylynnharman <dyer.taylynn@gmail.com>
2023-10-21 17:22:29 -0600

    windchill 1.0

---
+++ chamber/scripts/windchill.js
@@ -0,0 +1,22 @@
+function calculateWindChill() {
+    //get and store the input values
+    var temp = parseFloat(document.getElementById("temp").value);
+    var windspeed = parseFloat(document.getElementById("windspeed").value);
+
+    var windchill;
+    //check that temp and windspeed meet the specification limits (<=50°F and>3.0mph)
+    if (temp > 50 || windspeed <= 3) {
+        windchill = "N/A";
+    } else {
+        windchill = 35.74 + 0.6215 * temp - 35.75 * Math.pow(windspeed, 0.16) + 0.4275 * temp * Math.pow(windspeed, 0.16);
+    }
+    //update the windchill with the calculated windchill
+    document.querySelector("#windchill").textContent = windchill;
+}
+
+// Add input event listeners to both input fields.
+document.getElementById("temp").addEventListener("input", calculateWindChill);
+document.getElementById("windspeed").addEventListener("input", calculateWindChill);
+
+// Call the function once to calculate windchill initially.
+calculateWindChill();
