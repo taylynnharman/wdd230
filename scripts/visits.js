f5e1cd577b285a0fd371368d8b9abca83a5dde9f
taylynnharman <dyer.taylynn@gmail.com>
2023-10-26 10:36:14 -0600

    update homepage

--- scripts/visits.js
+++ scripts/visits.js
@@ -2,13 +2,13 @@
 const visitsDisplay = document.querySelector("#visits");
 
 // 2️⃣ Get the stored VALUE for the numVisits-ls KEY in localStorage if it exists. If the numVisits KEY is missing, then assign 0 to the numVisits variable.
-let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
+let numVisits = Number(localStorage.getItem("numVisits-ls")) || 0;
 
 // 3️⃣ Determine if this is the first visit or display the number of visits. We wrote this example backwards in order for you to think deeply about the logic.
 if (numVisits !== 0) {
 	visitsDisplay.textContent = numVisits;
 } else {
-	visitsDisplay.textContent = `This is your first visit. 🥳 Welcome!`;
+	visitsDisplay.textContent = `This is your first visit. Welcome!`;
 }
 
 // 4️⃣ increment the number of visits by one.
