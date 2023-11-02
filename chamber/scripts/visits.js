84bebc9852f7f52106b65dc3fd07cb4b83a62133
taylynnharman <dyer.taylynn@gmail.com>
2023-11-01 18:01:07 -0600

    table create

--- chamber/scripts/visits.js
+++ chamber/scripts/visits.js
@@ -1,15 +1,29 @@
+//initialize display element variable
 const visitsDisplay = document.querySelector("#visits");
 
-let numVisits = Number(localStorage.getItem("numVisits")) || 0;
+//get stored value for numVisits or set to 0
+let currentVisit = new Date();
 
-if (numVisits !==0) {
-    visitsDisplay.textContent = numVisits;
+if (numVisits == 0) {
+    //if this is their first visit
+    visitsDisplay.textContent = "Welcome! Let us know if you have any questions."
 } 
 
+else if (lastVisit - currentVisit ){
+    //If the amount of time between visits is less than a day
+    visitsDisplay.textContent = "Back so soon! Awesome!."
+}
+//
 else {
-    visitsDisplay.textContent = "Welcome! Let us know if you have any questions."
+    visitsDisplay.textContent = "Your last visit was" + {numVisits}+ "days ago."
 }
 
 numVisits++;
 
-localStorage.setItem("numVisitsDiscover-ls", numVisits);
+
+
+// If the number of days is 1, then change the language to "day" not "days"
+
+
+
+   
