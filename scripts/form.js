a2befc08a6aec61a3b5735af181096ef31c3acf7
taylynnharman <dyer.taylynn@gmail.com>
2023-11-02 22:31:30 -0600

    Update homepage

--- scripts/form.js
+++ scripts/form.js
@@ -18,3 +18,13 @@ function checkSame() {
 		p2.style.color = "#000";
 	}
 }
+const rangevalue = document.getElementById("rangevalue");
+const range = document.getElementById("r");
+
+// RANGE event listener
+range.addEventListener('change', displayRatingValue);
+range.addEventListener('input', displayRatingValue);
+
+function displayRatingValue() {
+    rangevalue.innerHTML = range.value;
+}
