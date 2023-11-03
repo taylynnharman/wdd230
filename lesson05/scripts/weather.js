16ad5cc15e19a229c327fbd496df051ff3d52405
taylynnharman <dyer.taylynn@gmail.com>
2023-11-02 19:15:12 -0600

    fix media ()

---
+++ lesson05/scripts/weather.js
@@ -0,0 +1,30 @@
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.64&units=imperial&appid=aaa802f065623b257e44b95ccc9e87c0'
+
+async function apiFetch() {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json()
+            console.log(data);
+            displayResults(data);
+        }
+        else {
+            throw Error (await response.text());
+        }
+    } catch (error) {
+            console.log(error);
+    }
+}
+apiFetch();
+
+function displayResults(data) {
+    currentTemp.innerHTML = `${data.main.temp}°F`;
+    const iconSrc = 'https://openweathermap.org/img/w/${data.weather[0].icon}.png';//weather icon not working
+    let desc = data.weather[0].description;
+    weatherIcon.setAttribute('src', iconSrc);
+    weatherIcon.setAttribute('alt', desc);
+    captionDesc.textContent = desc;
+}
