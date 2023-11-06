d75b06d3bb4f7be5d05514183a552d24b2287402
taylynnharman <dyer.taylynn@gmail.com>
2023-11-06 13:48:17 -0700

    Homepage Update

--- chamber/scripts/weather.js
+++ chamber/scripts/weather.js
@@ -1,10 +1,33 @@
-const temp = document.querySelector('#temp').innerHTML;
-const windspeed = document.querySelector('#windspeed').innerHTML;
-const windchill = document.querySelector('#windchill');
+// Set variables 
+const currentTemp = document.querySelector('#temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
 
-if (temp > 50 || windspeed <= 3) {
-    windChill = "N/A";
-} else {
-    windChill = 35.74 + 0.6215 * temp - 35.75 * windspeed **0.16 + 0.4275 * temp * windspeed **0.16;
+
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=40.4249&lon=111.7979&units=imperial&appid=aaa802f065623b257e44b95ccc9e87c0'
+
+async function apiFetch() {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json()
+            console.log(data);
+            displayResults(data);
+        }
+        else {
+            throw Error (await response.text());
+        }
+    } catch (error) {
+            console.log(error);
+    }
 }
-windchill.innerText = `${windChill.toFixed(2)} °F`;
+apiFetch();
+
+function displayResults(data) {
+    currentTemp.innerHTML = `${data.main.temp}°F`;
+    const iconSrc = 'https://openweathermap.org/img/w/${data.weather[0].icon}.png';//weather icon not working
+    let desc = data.weather[0].description;
+    weatherIcon.setAttribute('src', iconSrc);
+    weatherIcon.setAttribute('alt', desc);
+    captionDesc.textContent = desc;
+}
