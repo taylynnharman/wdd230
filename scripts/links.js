131c94ff23f6cb025cf668f570ddd7b91bebb5da
taylynnharman <dyer.taylynn@gmail.com>
2023-11-17 17:29:00 -0700

    Link.js update

--- scripts/links.js
+++ scripts/links.js
@@ -1,13 +1,18 @@
-// const baseURL = "https://taylynnharman.github.io/wdd230/";
-// const linksURL = "https://taylynnharman.github.io/wdd230/data/links.json";
-// const activity = document.querySelector('#activity');
+const baseURL = "https://taylynnharman.github.io/wdd230/";
+const linksURL = "https://taylynnharman.github.io/wdd230/data/links.json";
+const activity = document.querySelector('#activity');
 
-// async function getLinks() {
-//     const response = await fetch(linksURL);
-//     const data = await response.json();
-//     console.log(data);
-//     displayLinks(data.links); 
-// }
+async function getLinks() {
+    const response = await fetch(linksURL);
+    const data = await response.json();
+    console.log(data);
+    displayLinks(data.lessons);
+}
+const displayLinks = (lessons) => {
+    let heading = document.createElement('h3');
+    heading.textContent = "Learning Activities";
+    activity.appendChild(heading);
+
 
 // const displayLinks = (links) => {
 //     let heading = document.createElement('h3');
@@ -30,28 +35,16 @@
 
 // getLinks();
 
-const baseURL = "https://taylynnharman.github.io/wdd230/";
-const linksURL = "https://taylynnharman.github.io/wdd230/data/links.json";
-const activity = document.querySelector('#activity');
 
-async function getLinks() {
-    const response = await fetch(linksURL);
-    const data = await response.json();
-    console.log(data);
-    displayLinks(data.lessons);
-}
 
-const displayLinks = (lessons) => {
-    let heading = document.createElement('h3');
-    heading.textContent = "Learning Activities";
-    activity.appendChild(heading);
+
 
     lessons.forEach((lesson) => {
         let lessonHeading = document.createElement('h4');
         lessonHeading.textContent = `Lesson ${lesson.lesson}`;
         activity.appendChild(lessonHeading);
 
-        lesson.links.forEach((link, index) => {
+        lesson.links.forEach((link) => {
             let listItem = document.createElement('li');
             let a = document.createElement('a');
             a.href = `${baseURL}${link.url}`;
