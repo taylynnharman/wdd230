5fbc554eddc392c9f704785216afa3641201ac26
taylynnharman <dyer.taylynn@gmail.com>
2023-11-20 17:50:04 -0700

    w09 chamber

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -1,40 +1,50 @@
-const url = "../data/members.json"
+const source = "../data/members.json"
 const cards = document.querySelector('#cards');
 
-async function getProphetData() {
+async function getMemberData() {
     console.log('calling');
-    const response = await fetch(url);
+    const response = await fetch(source);
     const data = await response.json();
-    // console.table(data.prophets);
-    displayProphets(data.prophets)
+    // console.table(data.members);
+    displayMembers(data.members)
   }
 
-  getProphetData();
+  getMemberData();
 
-  const displayProphets = (prophets) => {
-    prophets.forEach((prophet) => {
+  const displayMembers = (members) => {
+    members.forEach((member) => {
         let card = document.createElement('section');
-        let fullName = document.createElement('h2'); 
-        let birthDate = document.createElement('p');
-        let birthPlace = document.createElement('p');
-        let portrait = document.createElement('img');
-        // Build the h2 content out to show the prophet's full name
-        fullName.textContent = `${prophet.name} ${prophet.lastname}`; // fill in the blank
+        let name = document.createElement('h2'); 
+        let logo = document.createElement('img');
+        let contacts = document.createElement('div'); 
+        let address = document.createElement('p');
+        let phone = document.createElement('p');
+        let website = document.createElement('p');
+        let membership = document.createElement('p');
+    
+        // Build the h2 content out to show the member's full name
+        name.textContent = `${member.name}`; 
+        address.textContent = `${member.name}`; 
+        phone.textContent = `${member.phone}`; 
+        website.textContent = `${member.url}`; 
+        membership.textContent = `${member.membership}`; 
+        name.textContent = `${member.name}`; 
+        // fill in the blank
         // Build the image portrait by setting all the relevant attributes
-        portrait.setAttribute('src', prophet.imageurl);
-        portrait.setAttribute('alt', `Portrait of ${prophet.name} ${prophet.lastname}`); // fill in the blank
-        portrait.setAttribute('loading', 'lazy');
-        portrait.setAttribute('width', '340');
-        portrait.setAttribute('height', '440');
-        // Build paragraph
-        birthDate.textContent = `Date of Birth: ${prophet.birthdate}`;
-        birthPlace.textContent = `Place of Birth: ${prophet.birthplace}`;
+        logo.setAttribute('src', member.image);
+        logo.setAttribute('alt', `${member.name} logo`); 
+        logo.setAttribute('loading', 'lazy');
+        logo.setAttribute('width', '340');
+        logo.setAttribute('height', '440');
+        // Build Div
+        contacts.appendChild(address); 
+        contacts.appendChild(phone); 
+        contacts.appendChild(website); 
+        contacts.appendChild(membership); 
         // Append the section(card) with the created elements
-        card.appendChild(fullName); 
-        card.appendChild(birthDate); 
-        card.appendChild(birthPlace); 
-        card.appendChild(portrait);
-
+        card.appendChild(name); 
+        card.appendChild(logo); 
+        card.appendChild(contacts); 
         cards.appendChild(card);
     });
   }
