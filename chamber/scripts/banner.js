de939bfb4b615ee318ad874b35df9800e2ae0203
taylynnharman <dyer.taylynn@gmail.com>
2023-11-25 16:43:35 -0700

    Fix Banner

--- chamber/scripts/banner.js
+++ chamber/scripts/banner.js
@@ -1,13 +1,15 @@
 const closeButton = document.querySelector('#closeBanner');
 const banner = document.querySelector('#banner');
-
-closeButton.addEventListener('click', () => {
-    const today = new Date().getDay();
+const today = new Date().getDay();
+    
     if (today >= 1 && today <= 3) {
         // Show the banner
-        const banner = document.getElementById("banner");
-        banner.style.display = "block";}
-        
-    banner.classList.toggle('close');
-    banner.style.display = "none";
-});
+        banner.style.display = "block";
+    } else {
+        // Hide the banner on other days
+        banner.style.display = "none";
+    }
+    closeButton.addEventListener('click', () => {
+        banner.classList.toggle('close');
+    });
+
