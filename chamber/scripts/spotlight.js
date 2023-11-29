aacccda3360ce361234d42ef15d682f641db50d2
taylynnharman <dyer.taylynn@gmail.com>
2023-11-28 18:04:20 -0700

    Responsive Spotlights done

--- chamber/scripts/spotlight.js
+++ chamber/scripts/spotlight.js
@@ -64,11 +64,47 @@ const memberCards = document.getElementById('memberCards');
             return container;
         }
 
-    function displaySpotlight(members) {
-        goldMembers = filterMembers(members);
-        console.log('Filtered gold members:', goldMembers);
-        memberCards.appendChild(createSpotlights());
-    }
+        function displaySpotlight(members) {
+            goldMembers = filterMembers(members);
+            console.log('Filtered gold members:', goldMembers);
+            
+            // Create spotlights and append them to memberCards
+            const spotlights = createSpotlights();
+            memberCards.appendChild(spotlights);
+        
+            // Call showSlides after creating spotlights
+            showSlides(slideIndex);
+        }
 
     // Call the function to fetch and display data
     getMemberData();
+    
+    // Slideshow
+    let slideIndex = 1;
+    showSlides(slideIndex);
+    
+    // Next/previous controls
+    function plusSlides(n) {
+      showSlides(slideIndex += n);
+    }
+    
+    // Thumbnail image controls
+    function currentSlide(n) {
+      showSlides(slideIndex = n);
+    }
+    
+    function showSlides(n) {
+      let i;
+      let slides = document.getElementsByClassName("mySlides");
+      let dots = document.getElementsByClassName("dot");
+      if (n > slides.length) {slideIndex = 1}
+      if (n < 1) {slideIndex = slides.length}
+      for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+      }
+      for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+      }
+      slides[slideIndex-1].style.display = "block";
+      dots[slideIndex-1].className += " active";
+    }
